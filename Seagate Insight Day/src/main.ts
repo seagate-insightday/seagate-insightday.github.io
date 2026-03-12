05072afc3e76c9b20d71d58d326f494cece1a5e9
Omer <57284289+mcnomer@users.noreply.github.com>
2026-03-12 22:35:26 +0000

    cleared out some old files and added boilerplate styles

--- Seagate Insight Day/src/main.ts
+++ Seagate Insight Day/src/main.ts
@@ -1,6 +1,8 @@
 import { mount } from 'svelte'
 import './app.css'
 import App from './App.svelte'
+import '@fontsource-variable/unbounded';
+import '@fontsource/poppins';
 
 const app = mount(App, {
   target: document.getElementById('app')!,
