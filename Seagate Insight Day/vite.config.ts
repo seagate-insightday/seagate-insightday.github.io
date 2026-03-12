83c366095980391f7a1a0f17910ffc723064ccdb
Omer <57284289+mcnomer@users.noreply.github.com>
2026-03-12 22:44:22 +0000

    added package for loading images

--- Seagate Insight Day/vite.config.ts
+++ Seagate Insight Day/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
+import { enhancedImages } from '@sveltejs/enhanced-img';
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    enhancedImages(),
+    svelte(),
+  ],
 })
